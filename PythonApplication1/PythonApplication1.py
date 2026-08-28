c7962c98714d30ae8d972d075374c62b2a10b28d
Shahar Kapash <shahar.kapash12@gmail.com>
2026-08-28 13:06:24 +0300

    fix to report

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -5,53 +5,95 @@ from urllib import response
 import requests
 results = {}
 def readfile(file_name):
+    print("Reading Opswat report..")          
+    print("-" * 40)
+    start_infected="--------Infected Files--------"
+    start_skipped="--------Skipped Files or Errors--------"
     with open(file_name,"r") as f:
-
+        for line in f:
+            if line.strip() == start_infected or line.strip() ==start_skipped:
+                break                
         for line in f:
          line = line.strip()
-         y=line.find(":")+1
-         x=line[:y-1]
-         results[line[y:]]={"name":x,"value":line[y:],"status":""}
-        with open("C:\\Users\\shaha\\source\\repos\\PythonApplication1\\jsonfile.json","w") as jsonfile:
-            json.dump(results,jsonfile,indent=2) #הכנסת האיבר לגייסון
+         if "Filename" in line:
+             y=line.find(":")+1
+             filename=line[y+1:]
+             line=f.readline()
+             line = line.strip()
+             y=line.find(":")+1
+             vhash=line[y+1:]
+             f.readline()
+             if vhash in results:
+                 results[vhash]["filename"].append(filename)
+             else:
+                 results[vhash]={"filename":[filename],"hash":vhash,"status":""}
+    with open("C:\\Users\\shaha\\source\\repos\\opswat_project\\jsonfile.json","w") as jsonfile:
+         json.dump(results,jsonfile,indent=2) #הכנסת האיבר לגייסון
 
+def whitelistloading():
+    #open approved file to set
+    print("Loading Whitelist files...")
+    print("-" * 40)
+    values={""}
+    with open("C:\\Users\\shaha\\source\\repos\\opswat_project\\approved.txt","r")as file:
+        for vhash in file:
+            vhash=vhash.strip()
+            values.add(vhash)
+    return values
+        
 def virustotalhash(jsonfile):
+    
     api="ee6ea0b4c9885f5b75ed60a4f16d9df873f6c3ddec9066c7d895412dcdddb437"
     
     headers = {
     "accept": "application/json",
     "x-apikey": api
 }
-    with open ("C:\\Users\\shaha\\source\\repos\\PythonApplication1\\jsonfile.json","r") as jsonfile:
+    with open ("C:\\Users\\shaha\\source\\repos\\opswat_project\\jsonfile.json","r") as jsonfile:
         file = json.load(jsonfile) #טעינה למשתנה כדי לעבוד 
-    with open ("C:\\Users\\shaha\\source\\repos\\PythonApplication1\\jsonfile.json","w") as jsonfile:
+    with open ("C:\\Users\\shaha\\source\\repos\\opswat_project\\jsonfile.json","w") as jsonfile:
+        values=whitelistloading()
+        error=False
         for fhash in file.values():
-            key=fhash.get("value")
+            key=fhash.get("hash")
             url=f"https://www.virustotal.com/api/v3/files/{key}"
-            print("Checking the hash == ",key)
-            try:
-                response = requests.get(url, headers=headers)
-                if response.status_code == 200:
-                    result_data = response.json()
-                    stats = result_data.get("data", {}).get("attributes", {})#.get("last_analysis_stats", {})
-                    malicious_count = stats.get("malicious", 0)
-                    print(stats.keys())
-                    print(f" ->{malicious_count}")
-                    fhash["status"]=stats
-                elif response.status_code == 404:
-                    print(" ->Hash not found.")
-                else:
-                    print(f" ->Connection failed: {response.status_code}")
-            except Exception as e:
-                print(f" -> תקלת חיבור: {e}")
+            print("Checking == ",key)
+            if key in values:
+                print("[WL]-> File is Whitelisted")
+                fhash["status"]="Whitelisted"
+            else:
+                try:
+                    response = requests.get(url, headers=headers)
+                    if response.status_code == 200:
+                       result_data = response.json()
+                       stats = result_data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+                       malicious_count = stats.get("malicious", 0)
+                       print("[VirusTotal] ->")
+                       for key, value in stats.items():
+                         print(f"{key:<20} | {value}")
+                       fhash["status"]=stats
+                    elif response.status_code == 404:
+                     print("[VirusTotal] -> Hash not found")
+                     print("[WL]         -> Hash not found")
+                     fhash["status"]= "N/A"
+                    else:
+                     print(f"[VirusTotal] ->Connection failed: {response.status_code}")
+                     error=True
+                except Exception as e:
+                   error=True
+                   print(f" ->ERROR: {e}")
             print("-" * 40)
-            time.sleep(5)
-        json.dump(file,jsonfile,indent=2)
+            time.sleep(4)
+        json.dump(file,jsonfile,indent=2) 
+    if error ==True:
+        print("NOTE! an error happend will runnin, some results may not appear")
     print("File saved!")          
 
 
 
-readfile("C:\\Users\\shaha\\source\\repos\\PythonApplication1\\pythontest.txt")
-virustotalhash("C:\\Users\\shaha\\source\\repos\\PythonApplication1\\jsonfile.json")
+filepath="C:\\Users\\shaha\\source\\repos\\opswat_project\\ex_file.txt"
+jsonfile="C:\\Users\\shaha\\source\\repos\\opswat_project\\jsonfile.json"
+readfile(filepath)
+virustotalhash(jsonfile)
 
     
